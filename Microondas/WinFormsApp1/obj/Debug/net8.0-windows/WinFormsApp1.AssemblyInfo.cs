6ebbe8c360202de3e3df19274f72123a5d99e556
Eduardo <97778100+eduardolima002@users.noreply.github.com>
2024-01-29 08:12:39 -0300

    Tirando bug do init

--- Microondas/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ Microondas/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a4fd5b97a2ae644ddb8bcb8ed5e76cb95a5c3ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495ffd3af4a17e5e2b5390b97c4c71fcb95d30a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
